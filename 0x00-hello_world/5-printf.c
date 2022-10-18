522af647148e3dbe8508024e6a05fea4e29a8d5f
prymux <georgewheatborn@gmail.com>
2022-10-18 05:46:01 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+
+/**
+ *
+ *  * main - Entry
+ *
+ *   * Return: Always 0
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+				return (0);
+
+}
