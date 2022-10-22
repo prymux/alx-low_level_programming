106ad9a11b228f8f828bb586bb9ee2394f5a71fc
prymux <georgewheatborn@gmail.com>
2022-10-21 17:58:13 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - check for character uppercase
+ * @c: variable test
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
