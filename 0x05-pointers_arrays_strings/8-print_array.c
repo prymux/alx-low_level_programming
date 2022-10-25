aa7d5ff9f854aa132afe24ffde66ff542489da80
prymux <georgewheatborn@gmail.com>
2022-10-24 21:18:12 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints in elements of an array
+ * @a: array
+ * @n: numbers of values to be opened
+ */
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		printf("%d",  a[t]);
+		if (t != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
